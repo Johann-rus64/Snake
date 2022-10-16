013b0fe701bfb2ef954650622827cce960becdf9
ooze <grigorianna@gmail.com>
2022-10-16 18:56:35 +0400

    List

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -12,6 +12,19 @@ internal class Program
        
         p2.Draw();
 
+        List<int> numList = new List<int>();
+        numList.Add(0);
+        numList.Add(1);
+        numList.Add(2);
+
+        int x = numList[0];
+        int y = numList[1];
+        int z = numList[2];
+
+        foreach (int i in numList)
+        {
+            Console.WriteLine(i);
+        }
 
         Console.ReadLine();
     }
